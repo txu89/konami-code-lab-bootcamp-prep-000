956ab0879b912ecf2c7eed624d0a271e5e2df66c
Tom Xu <tomxu89@gmail.com>
2018-01-16 06:27:24 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,22 +4,21 @@ function init() {
   
   document.addEventListener('keydown', onKeyDownHandler)
   
-}
-
-let i = 0
+  let i = 0
   
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which)
-  console.log(e.detail + ' ' + e.which)
-  if (key === code[i]) {
-    i++
-    
-    if (i === code.length) {
-      alert('congrats, you entered the code')
-      i = 0 
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which)
+    console.log(e.detail + ' ' + e.which)
+    if (key === code[i]) {
+      i++
+      
+      if (i === code.length) {
+        alert('congrats, you entered the code')
+        i = 0 
+      }
+    } else {
+      i = 0
     }
-  } else {
-    i = 0
   }
 }
 
