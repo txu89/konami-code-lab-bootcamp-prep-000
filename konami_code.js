0f4e1f97651a3cce449c31ceb58836e20ddf4a1b
Tom Xu <tomxu89@gmail.com>
2018-01-16 06:23:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  let i = 0
+  
+  document.addEventListener('keydown', onKeyDownHandler)
+  
+}
+
+let i = 0
   
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which)
@@ -18,6 +23,5 @@ function init() {
       i = 0
     }
   }
-}
 
 init()
